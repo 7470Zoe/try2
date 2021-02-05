ac56001356d487a91292353c5a43b4fd0b674a79
syt <m13058178050@163.com>
2021-02-05 11:57:48 +0800

    控制tank的方向

--- src/com/syt/TankFrame.java
+++ src/com/syt/TankFrame.java
@@ -32,24 +32,62 @@ public class TankFrame extends Frame {
 	@Override
 	public void paint(Graphics g) {
 		// TODO 自动生成的方法存根
-		System.out.println("Graphics ");
+//		System.out.println("Graphics ");
 		g.fillRect(x, y, 50, 50);
-		x+=10;
-		y+=10;
+//		x+=10;
+//		y+=10;
 		
 	}
 	
 	//键盘监听处理类   是一个命名的内部类
 	class MyKeyListener extends KeyAdapter{
+		boolean bL= false;
+		boolean bU= false;
+		boolean bR= false;
+		boolean bD= false;
 
 		@Override
 		public void keyPressed(KeyEvent e) {
+			int key = e.getKeyCode();
+			switch(key) {
+			case KeyEvent.VK_LEFT:
+				bL= true;
+				break;
+			case KeyEvent.VK_UP:
+				bU= true;
+				break;
+			case KeyEvent.VK_RIGHT:
+				bR=true;
+				break;
+			case KeyEvent.VK_DOWN:
+				bD=true;
+				break;
+			default:
+				break;
+			}
 			
 		}
 
+		//抬起键盘的时候应该给设置为false
 		@Override
 		public void keyReleased(KeyEvent e) {
-		
+			int key = e.getKeyCode();
+			switch(key) {
+			case KeyEvent.VK_LEFT:
+				bL= false;
+				break;
+			case KeyEvent.VK_UP:
+				bU= false;
+				break;
+			case KeyEvent.VK_RIGHT:
+				bR=false;
+				break;
+			case KeyEvent.VK_DOWN:
+				bD=false;
+				break;
+			default:
+				break;
+			}
 		}
 		
 	}
